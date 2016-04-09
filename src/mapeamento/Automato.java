3ce206c69be7243d424012f2deb29f43cc10b77b
Gustavo <Gustavo@192.168.1.9>
2016-04-09 19:15:47 -0300

    SISTOM-4

---
+++ src/mapeamento/Automato.java
@@ -0,0 +1,149 @@
+package mapeamento;
+
+import mapeamento.beans.ImagemProcessada;
+import mapeamento.beans.Talhao;
+import mapeamento.beans.Tomates;
+
+/**
+ *
+ * @author Gustavo
+ */
+public class Automato {
+
+        private final Talhao talhao;
+	private final int X;
+        private final int Y;
+        private Tomates [][] matriz;
+
+    /**
+     *
+     * @param talhao
+     */
+    public Automato(Talhao talhao) {
+        this.talhao = talhao;
+        this.X = talhao.getQtd_TomatesPorLinhas();
+        //considerando que cada rua tem 2 linhas.
+        this.Y = talhao.getQtdRuas() * 2;
+        this.matriz = new Tomates [X][Y];
+    }
+
+	
+	public void inicializa(Tomates matriz[][])//recebe valor inicial
+	{
+		int i, j, max_X, max_Y;
+                
+		max_X = this.X-1;
+                max_Y = this.Y-1;
+     
+		for(i=0;i < max_X; i++){
+			for(j=0; j < max_Y; j++){
+				//matriz[i][j] = (int)(Math.random()*1.5);
+                                this.matriz[i][j] = matriz[i][j];
+			}
+		}
+               
+	}
+	
+    /**
+     *
+     */
+    public void imprime(){
+		
+		int i, j, max_X, max_Y;
+                max_X = this.X-1;
+                max_Y = this.Y-1;
+		
+		for(i=0; i <max_X; i++){
+			for(j=0; j < max_Y; j++){
+                            ImagemProcessada imagemProcessada = matriz[i][j].getImagemProcessada();
+                            int estado = imagemProcessada.getEstado();
+                            System.out.print(estado);
+				
+				
+			}
+			System.out.println();
+		}
+		
+	}
+	
+    /**
+     *
+     * @param i
+     * @param j
+     * @return
+     */
+    public int vizinhos (int i, int j){
+            //VIZINHAÇA EM 2D
+            int min = 0;
+            int max_X = this.X-1;
+            int max_Y = this.Y-1;
+            int cantoEsquerdoSuperior = (i!=min && j!=min) ? matriz[i - 1][j - 1].getImagemProcessada().getEstado(): 0;
+            int meioSuperior = (i!=min) ? matriz[i - 1][j].getImagemProcessada().getEstado() : 0;
+            int cantoDireitoSuperior = (i!=min && j!=max_Y) ? matriz[i - 1][j + 1].getImagemProcessada().getEstado() : 0;
+            int meioEsquerdo = (j!=min) ? matriz[i][j - 1].getImagemProcessada().getEstado() : 0;
+            int meioDireito = (j!=max_Y) ? matriz[i][j + 1].getImagemProcessada().getEstado() : 0;
+            int cantoEsquerdoInferior = (i!=max_X && j!=min) ? matriz[i + 1][j - 1].getImagemProcessada().getEstado() : 0;
+            int meioInferior = (i!=max_X) ? matriz[i + 1][j].getImagemProcessada().getEstado(): 0 ;
+            int cantoDireitoInferior = (i!=max_X && j!=max_Y) ? matriz[i + 1][j + 1].getImagemProcessada().getEstado() : 0;
+            
+            //vizinhaca é a soma dos estados
+            int vizinhaca = cantoEsquerdoSuperior+meioSuperior+cantoDireitoSuperior+meioEsquerdo+meioDireito+cantoEsquerdoInferior+meioInferior+cantoDireitoInferior;
+            return  vizinhaca;
+	}
+	
+    /**
+     *
+     * @return
+     */
+    public Tomates [][] iteracao(){
+		Tomates [][] aux = new Tomates [X][Y];
+		int i, j, max_X, max_Y;
+                max_X = this.X-1;
+                max_Y = this.Y-1;
+		
+		for (i = 0; i < max_X; i++){
+			for (j = 0; j < max_Y; j++){
+                            ImagemProcessada imagemProcessada = matriz[i][j].getImagemProcessada();
+                            int estado = imagemProcessada.getEstado();
+                               // COLOCAR REGRAS AQUI
+                               aux[i][j] = matriz[i][j];
+                               ImagemProcessada imagemProcessadaForAux= aux[i][j].getImagemProcessada();
+				if( estado == 1){
+					if((vizinhos(i,j) < 2) || (vizinhos(i,j) > 3)){
+                                           
+                                            imagemProcessadaForAux.setEstado(2);
+					}
+					else{
+						imagemProcessadaForAux.setEstado(1);
+					}
+				}
+				else{
+					if( vizinhos(i,j) == 3 ){
+						imagemProcessadaForAux.setEstado(1);
+					}
+					else{
+						imagemProcessadaForAux.setEstado(2);
+					}
+				}
+			}
+		}
+		return aux;
+	}
+	
+    /**
+     *
+     * @param quant
+     */
+    public void simulaPropagação(int quant){
+		
+		int i;
+		
+		for(i=0; i< quant; i++){
+                    System.out.println("Iteração: "+ (i+1));
+			imprime();
+			matriz = iteracao();
+		}
+	
+	}
+	
+}
--- src/mapeamento/Preencher_Variaveis.java
+++ src/mapeamento/Preencher_Variaveis.java
@@ -36,15 +36,10 @@ public class Preencher_Variaveis extends javax.swing.JPanel {
         jTextField2 = new javax.swing.JTextField();
         jLabel3 = new javax.swing.JLabel();
         jTextField3 = new javax.swing.JTextField();
-        jLabel4 = new javax.swing.JLabel();
-        jTextField4 = new javax.swing.JTextField();
-        jLabel5 = new javax.swing.JLabel();
         jLabel6 = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
-        jComboBox1 = new javax.swing.JComboBox();
         jLabel7 = new javax.swing.JLabel();
         jLabel8 = new javax.swing.JLabel();
-        jLabel9 = new javax.swing.JLabel();
         jLabel10 = new javax.swing.JLabel();
         jLabel11 = new javax.swing.JLabel();
         jTextField1 = new javax.swing.JTextField();
@@ -68,32 +63,17 @@ public class Preencher_Variaveis extends javax.swing.JPanel {
 
         jLabel3.setText(" índice pluviométrico:");
 
-        jLabel4.setText("Velocidade Média do Vento:");
-
-        jTextField4.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTextField4ActionPerformed(evt);
-            }
-        });
-
-        jLabel5.setText("Direção do vento:");
-
         jLabel6.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
         jLabel6.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel6.setText("Fatores determinantes para disseminação da REQUEIMA");
+        jLabel6.setText("Preencher Fatores Determinantes para a Simulação");
 
         jButton1.setText("Iniciar Simulação");
-
-        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Norte", "Nordeste", "Noroeste", "Sul", "Suldeste", "Suldoeste", "Leste", "Oeste" }));
 
         jLabel7.setFont(new java.awt.Font("Tahoma", 2, 10)); // NOI18N
         jLabel7.setText("ºC");
 
         jLabel8.setFont(new java.awt.Font("Tahoma", 2, 10)); // NOI18N
         jLabel8.setText("mm/m²");
-
-        jLabel9.setFont(new java.awt.Font("Tahoma", 2, 10)); // NOI18N
-        jLabel9.setText("km/h");
 
         jLabel10.setFont(new java.awt.Font("Tahoma", 2, 10)); // NOI18N
         jLabel10.setText("%");
@@ -113,13 +93,11 @@ public class Preencher_Variaveis extends javax.swing.JPanel {
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jLabel6, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 400, Short.MAX_VALUE)
+            .addComponent(jLabel6, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
             .addGroup(layout.createSequentialGroup()
-                .addGap(29, 29, 29)
+                .addGap(62, 62, 62)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                     .addComponent(jLabel11)
-                    .addComponent(jLabel5)
-                    .addComponent(jLabel4)
                     .addComponent(jLabel3)
                     .addComponent(jLabel2)
                     .addComponent(jLabel1))
@@ -129,15 +107,12 @@ public class Preencher_Variaveis extends javax.swing.JPanel {
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                             .addComponent(jTextField2, javax.swing.GroupLayout.DEFAULT_SIZE, 59, Short.MAX_VALUE)
                             .addComponent(jTextField3)
-                            .addComponent(jTextField4)
                             .addComponent(textfield_Humid))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel9)
                             .addComponent(jLabel10)
                             .addComponent(jLabel7, javax.swing.GroupLayout.PREFERRED_SIZE, 24, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(jLabel8, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                    .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
@@ -169,21 +144,12 @@ public class Preencher_Variaveis extends javax.swing.JPanel {
                     .addComponent(jLabel3)
                     .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel8))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel4)
-                    .addComponent(jTextField4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel9))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel5)
-                    .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel11)
                     .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel13))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 36, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 101, Short.MAX_VALUE)
                 .addComponent(jButton1)
                 .addContainerGap())
         );
@@ -192,10 +158,6 @@ public class Preencher_Variaveis extends javax.swing.JPanel {
     private void jTextField2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField2ActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_jTextField2ActionPerformed
-
-    private void jTextField4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField4ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jTextField4ActionPerformed
 
     private void textfield_HumidActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_textfield_HumidActionPerformed
         // TODO add your handling code here:
@@ -208,23 +170,18 @@ public class Preencher_Variaveis extends javax.swing.JPanel {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
-    private javax.swing.JComboBox jComboBox1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel10;
     private javax.swing.JLabel jLabel11;
     private javax.swing.JLabel jLabel13;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
     private javax.swing.JLabel jLabel7;
     private javax.swing.JLabel jLabel8;
-    private javax.swing.JLabel jLabel9;
     private javax.swing.JTextField jTextField1;
     private javax.swing.JTextField jTextField2;
     private javax.swing.JTextField jTextField3;
-    private javax.swing.JTextField jTextField4;
     private javax.swing.JTextField textfield_Humid;
     // End of variables declaration//GEN-END:variables
 }
