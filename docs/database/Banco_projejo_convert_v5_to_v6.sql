b57d9cb7343797946ffdcb67334ed3c773b34710
gustavo.sucupira <gustavo.sucupira@MR-PLOW.minds.local>
2016-10-12 23:33:04 -0300

    fix

--- docs/database/Banco_projejo_convert_v5_to_v6.sql
+++ docs/database/Banco_projejo_convert_v5_to_v6.sql
@@ -14,8 +14,8 @@ START TRANSACTION;
 
 ALTER TABLE `dados_meteorologicos` 
 CHANGE COLUMN `umidade` `umidade` DOUBLE NOT NULL ,
-ADD COLUMN `temp_min` DOUBLE NOT NULL AFTER `flag_chuva`,
-ADD COLUMN `temp_max` DOUBLE NOT NULL AFTER `temp_mini`,
+ADD COLUMN `temp_mini` DOUBLE NOT NULL AFTER `flag_chuva`,
+ADD COLUMN `temp_max` DOUBLE NOT NULL AFTER `temp_min`,
 ADD COLUMN `precipitacao` DOUBLE NOT NULL AFTER `temp_max`;
 
 /*INSERTs*/
